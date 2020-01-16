3e4de6032ffba210468e7e76f816a8293f3b3e20
Elaia Raj <elaia.raj@gmail.com>
2020-01-16 15:16:31 +0800

    fixed docker id

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
-docker build -t elrajgit/multi-client:latest -t elrajgit/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t elrajgit/multi-server:latest -t elrajgit/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t elrajgit/multi-worker:latest -t elrajgit/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push elrajgit/multi-client:latest
-docker push elrajgit/multi-server:latest
-docker push elrajgit/multi-worker:latest
+docker build -t elrajdocker/multi-client:latest -t elrajdocker/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t elrajdocker/multi-server:latest -t elrajdocker/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t elrajdocker/multi-worker:latest -t elrajdocker/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push elrajdocker/multi-client:latest
+docker push elrajdocker/multi-server:latest
+docker push elrajdocker/multi-worker:latest
 
-docker push elrajgit/multi-client:$SHA
-docker push elrajgit/multi-server:$SHA
-docker push elrajgit/multi-worker:$SHA
+docker push elrajdocker/multi-client:$SHA
+docker push elrajdocker/multi-server:$SHA
+docker push elrajdocker/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/client-deployment client=elrajgit/multi-client:$SHA
-kubectl set image deployments/server-deployment server=elrajgit/multi-server:$SHA
-kubectl set image deployments/worker-deployment worker=elrajgit/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=elrajdocker/multi-client:$SHA
+kubectl set image deployments/server-deployment server=elrajdocker/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=elrajdocker/multi-worker:$SHA
